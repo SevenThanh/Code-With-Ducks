4b79630a6f4ccf0106b60b4fcd7f104d70fa7802
Johan Nguyen <thaithanhace@gmail.com>
2024-07-04 18:41:09 -0500

    finished 2/3 of homepage

--- front-end/src/pages/Body.js
+++ front-end/src/pages/Body.js
@@ -8,10 +8,13 @@ function Body() {
     <section className='home-page'>
       <div className="content">
         <h2>Learn to Code <br /> with <span id="school-name">Stevens</span></h2>
+        <h3>Join us today to learn how to code</h3>
+        <button className='button2'>Get Started</button>
       </div>
       <div id="mascot">
         <img src="/mascot.png" alt="Code with Ducks Mascot" />
       </div>
+
     </section>
   );
 }
