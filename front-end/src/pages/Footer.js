0aac8d450dd4416954b0a862a5e3098e18717a31
Kevin Cheng <kev72eat@gmail.com>
2024-07-10 18:49:51 -0400

    build footer, have big tech scroll

--- front-end/src/pages/Footer.js
+++ front-end/src/pages/Footer.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import google from "../assets/google.png"
+import apple from "../assets/apple.png"
+import microsoft from "../assets/microsoft.png"
+import meta from "../assets/meta.png"
+import nvdia from "../assets/nvdia.png"
+import amazon from "../assets/amazon.webp"
+import oracle from "../assets/oracle.png"
+import '../styles/Footer.css'
 
 function Footer() {
   return (
     <footer>
+      <header id="footer-title">
+        <h2>Our Students</h2>
+        <span>intern at</span>
+      </header>
+      <div id="logos">
+        <img id="google" src={google} alt="microsoft logo" />
+        <img id="apple" src={apple} alt="microsoft logo" />
+        <img id="microsoft" src={microsoft} alt="microsoft logo" />
+        <img id="meta" src={meta} alt="microsoft logo" />
+        <img id="nvdia" src={nvdia} alt="microsoft logo" />
+        <img id="amazon" src={amazon} alt="microsoft logo" />
+        <img id="oracle" src={oracle} alt="microsoft logo" />
+        <img id="google" src={google} alt="microsoft logo" />
+        <img id="apple" src={apple} alt="microsoft logo" />
+        <img id="microsoft" src={microsoft} alt="microsoft logo" />
+        <img id="meta" src={meta} alt="microsoft logo" />
+        <img id="nvdia" src={nvdia} alt="microsoft logo" />
+        <img id="amazon" src={amazon} alt="microsoft logo" />
+        <img id="oracle" src={oracle} alt="microsoft logo" />
+      </div>
       <p>© 2024 Code with Ducks. All rights reserved.</p>
     </footer>
   );
