0f9bce0621805515fc707f5c6eadce016f0750ed
Kevin Cheng <kev72eat@gmail.com>
2024-08-02 14:27:59 -0400

    change css directives to use more classnames rather than the tagname itself

--- front-end/src/Hero.tsx
+++ front-end/src/Hero.tsx
@@ -2,9 +2,9 @@ import "./styles/Hero.css"
 
 export function Hero() {
     return (
-        <div id="wrapper">
-            <main>
-                <h1 id="title">Learn to Code</h1>
+        <div id="hero-wrapper">
+            <main id="hero">
+                <h1>Learn to Code</h1>
                 <p>Join us today to learn how to code at Stevens Institute of Technology</p>
                 <button onClick={() => console.log("peepee")}>Apply</button>
             </main>
