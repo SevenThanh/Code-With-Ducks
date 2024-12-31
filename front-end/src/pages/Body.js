d1cf75fa07a9056a718fedff8725494024523412
Johan Nguyen <thaithanhace@gmail.com>
2024-12-30 22:25:34 -0600

    update

--- front-end/src/pages/Body.js
+++ front-end/src/pages/Body.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/App.css'
 import '../styles/body.css'
+import goals from '../assets/goals.png'
 
 
 function Body() {
@@ -8,7 +9,7 @@ function Body() {
     <section className='goals'>
       <h2 className="goals-title">Goals</h2>
       <div className="goals-container">
-        {/* <img src={goals} alt="Goals" className="goals-image" /> */}
+        <img src={goals} alt="Goals" className="goals-image" />
         <div className="goals-text">
           <p>At Code with Ducks, we aim to provide educational empowerment, increase STEM inclusivity, build community and networks, and create accessible learning opportunities to high school students</p>
         </div>
